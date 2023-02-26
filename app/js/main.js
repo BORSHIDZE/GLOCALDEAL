9db979be1fb135fc4c64927c24c559f82208c32d
Your Name~ <prototip1029@gmail.com>
2023-02-26 21:43:30 +0600

    added adaptiv on main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
 $(function name(params) {
   $('.burger').on('click', function () {
     $('.menu__list').toggleClass('menu__list--active');
+    $('.header__logo').toggleClass('header__logo--active');
+    $('.burger').toggleClass('burger--active');
+    $('.header__top').toggleClass('header__top--active'); 
+    $('.dropdown').toggleClass('dropdown-center');
+    $('.header__bottom').toggleClass('header__bottom--active');
   })
 })
