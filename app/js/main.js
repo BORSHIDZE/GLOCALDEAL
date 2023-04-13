826d08cd5f0527aa6580b4d85add67a8b3a00f31
Your Name~ <prototip1029@gmail.com>
2023-04-13 14:37:20 +0600

    сделал все, и проверил. Косяков на страницах нету, хыхы

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,8 @@ $(function name(params) {
     $('.menu__list').toggleClass('menu__list--active');
     $('.header__menu').toggleClass('header__menu--active');
     $('.dropdown').toggleClass('dropdown-center');
-    $('.header__logo_burger').toggleClass('header__logo--active')
+    $('.header__logo_burger').toggleClass('header__logo--active');
+    $('.decor-order__form_label__radio').toggleClass('decor-order__form_label__radio--active');
+    $('.delivery').toggleClass('delivery--active');
   })
 })
